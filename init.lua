f1dbeed0ab7cbd15c17785ee177077a0d9c5a4da
NovaX <lupogovirus@gmail.com>
2025-07-31 21:26:31 +0200

    created mod

---
+++ init.lua
@@ -0,0 +1,125 @@
+local MAX_TEXTS = tonumber(minetest.settings:get("textmod_max_texts") or 100)
+local DEFAULT_RANGE = tonumber(minetest.settings:get("textmod_default_range") or 200)
+
+local text_entities = {}
+
+local function parse_color(c)
+    if c and c:match("^#%x%x%x%x%x%x$") then
+        return c
+    end
+    return "#FFFFFF"
+end
+
+local function parse_pos(x, y, z)
+    return {x = tonumber(x), y = tonumber(y), z = tonumber(z)}
+end
+
+minetest.register_entity("textmod:floating_text", {
+    initial_properties = {
+        physical = false,
+        collide_with_objects = false,
+        pointable = false,
+        visual = "cube",
+        textures = {"blank.png", "blank.png", "blank.png", "blank.png", "blank.png", "blank.png"},
+        visual_size = {x = 0, y = 0},
+        static_save = false,
+        nametag = "",
+        nametag_color = "#FFFFFF",
+        show_on_minimap = false,
+    },
+
+    on_activate = function(self)
+        self.object:set_armor_groups({immortal = 1})
+    end,
+
+    set_text = function(self, txt, color)
+        self.object:set_nametag_attributes({
+            text = txt,
+            color = color,
+            bgcolor = "#00000000"
+        })
+    end,
+})
+
+minetest.register_chatcommand("text", {
+    params = "<x y z> <#color> [range] \"text\"",
+    description = "Spawn floating text at coordinates or above yourself",
+    privs = {shout = true},
+    func = function(name, param)
+        local text = param:match("\"(.-)\"")
+        if not text then
+            return false, "Text must be in quotes."
+        end
+
+        text = text:gsub("\\n", "\n")
+
+        local args = {}
+        for token in param:gmatch("%S+") do table.insert(args, token) end
+
+        if #text_entities >= MAX_TEXTS then
+            return false, "Text limit reached (" .. MAX_TEXTS .. ")"
+        end
+
+        local pos
+        local color
+        local range
+
+        if tonumber(args[1]) and tonumber(args[2]) and tonumber(args[3]) then
+            -- coordinates given
+            pos = parse_pos(args[1], args[2], args[3])
+            color = parse_color(args[4])
+            range = tonumber(args[5]) or DEFAULT_RANGE
+        else
+            -- no coordinates: spawn above self
+            local player = minetest.get_player_by_name(name)
+            if not player then return false, "You are not a valid player." end
+            pos = vector.add(player:get_pos(), {x = 0, y = 2, z = 0})
+            color = parse_color(args[1])
+            range = tonumber(args[2]) or DEFAULT_RANGE
+        end
+
+        local entity = minetest.add_entity(pos, "textmod:floating_text")
+        if entity then
+            local id = #text_entities + 1
+            entity:get_luaentity():set_text(text, color)
+            text_entities[id] = {obj = entity, pos = pos}
+            return true, "Text spawned with ID #" .. id
+        end
+
+        return false, "Failed to spawn text."
+    end
+})
+
+minetest.register_chatcommand("remove_text", {
+    params = "<x y z | ID>",
+    description = "Remove text by position or ID",
+    privs = {shout = true},
+    func = function(name, param)
+        local args = {}
+        for token in param:gmatch("%S+") do table.insert(args, token) end
+
+        if #args == 1 and tonumber(args[1]) then
+            local id = tonumber(args[1])
+            local data = text_entities[id]
+            if data and data.obj and data.obj:get_luaentity() then
+                data.obj:remove()
+                text_entities[id] = nil
+                return true, "Text ID #" .. id .. " removed"
+            end
+            return false, "ID not found"
+        elseif #args == 3 then
+            local pos = parse_pos(args[1], args[2], args[3])
+            local removed = 0
+            for id, data in pairs(text_entities) do
+                if vector.equals(vector.round(data.pos), vector.round(pos)) then
+                    data.obj:remove()
+                    text_entities[id] = nil
+                    removed = removed + 1
+                end
+            end
+            return true, removed .. " text(s) removed at position"
+        end
+
+        return false, "Usage: /remove_text <ID> or /remove_text <x y z>"
+    end
+})
